14c8d4ee68a8c4bbd19feb8e1a629951c46b9872
camlatimer <camlatimer@gmail.com>
2017-07-30 17:42:46 -0400

    decided on overall vibe, layout, feel, and responsive breakpoints for home/landing page

--- js/entry.js
+++ js/entry.js
@@ -1,2 +1,14 @@
 const go = `let's go...`
 console.log(go);
+
+const navIcons = document.querySelectorAll('.nav__icon, .navM__icon');
+const mobileMenu = document.querySelector('.navM');
+const navMLinks = document.querySelectorAll('.navM__link, .infoIcon--nav');
+navIcons.forEach(function(icon){
+  icon.addEventListener('click', function(){
+    mobileMenu.classList.toggle('showNavM');
+    navMLinks.forEach(function(link){
+      link.classList.toggle('showNavMLinks');
+    })
+  })
+})
