9344ec2fca980c9e1952a8ab1b57c59b1e33a4cb
camlatimer <camlatimer@gmail.com>
2017-07-31 15:54:06 -0400

    adds responsive style tweaks to header, adds animations to anchor links, slow scroll to headline

--- js/entry.js
+++ js/entry.js
@@ -1,14 +1,56 @@
-const go = `let's go...`
-console.log(go);
+window.onload = function(){
+  const navIcons = document.querySelectorAll('.nav__icon, .navM__icon');
+  const mobileMenu = document.querySelector('.navM');
+  const navLinksWide = document.querySelectorAll('.nav__link a');
+  const navMLinks = document.querySelectorAll('.navM__link, .infoIcon--nav');
+  const titleWrapper = document.querySelector('.headline__wrapper');
+  const titleTop =  parseInt(window.getComputedStyle(titleWrapper, null).top, 10);
+  const tastyRow = document.querySelector('.tastyRow');
+  const navFullWidth = document.querySelector('.navFull');
 
-const navIcons = document.querySelectorAll('.nav__icon, .navM__icon');
-const mobileMenu = document.querySelector('.navM');
-const navMLinks = document.querySelectorAll('.navM__link, .infoIcon--nav');
-navIcons.forEach(function(icon){
-  icon.addEventListener('click', function(){
-    mobileMenu.classList.toggle('showNavM');
-    navMLinks.forEach(function(link){
-      link.classList.toggle('showNavMLinks');
+
+  // toggle mobile nav
+  navIcons.forEach(function(icon){
+    icon.addEventListener('click', function(){
+      mobileMenu.classList.toggle('showNavM');
+      navMLinks.forEach(function(link){
+        link.classList.toggle('showNavMLinks');
+      })
     })
   })
-})
+
+  // slow / delayed parallax-style scroll for site title
+   document.addEventListener('scroll', function(){
+     parllaxHeadlineControl();
+     navBgControl();
+   })
+
+   function parllaxHeadlineControl(){
+     const scrollDistance = window.scrollY;
+     const topSet = (titleTop + (scrollDistance / 2));
+     titleWrapper.style.top = topSet + 'px';
+   }
+
+  function navBgControl(){
+    const tastyRowTop = tastyRow.getBoundingClientRect().top;
+    let linkColor;
+    function linkColorSwitch(link){
+        link.style.color = linkColor;
+    }
+    if (window.innerWidth > 640){
+      if(tastyRowTop < 200){
+        navFullWidth.style.backgroundColor = 'white';
+        linkColor = 'black';
+        navLinksWide.forEach(linkColorSwitch)
+      } else if(tastyRowTop > 200){
+        navFullWidth.style.backgroundColor = 'transparent';
+        linkColor = 'white';
+        navLinksWide.forEach(linkColorSwitch)
+      }
+    }
+
+  }
+
+
+
+}
